4afd653a663455a468beb3024035ab1b751a866b
jobisne <olajobinurudeen2013@gmail.com>
2021-07-11 08:49:00 +0100

    port modify

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const { MONGODB } = require('./config');
-const PORT = process.env.port || 4000;
+const PORT = process.env.PORT || 4000;
 
 const typeDefs = require('./graphql/typeDefs');
 const resolvers = require('./graphql/resolvers');
